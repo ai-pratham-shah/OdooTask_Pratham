f61eb799132a2cf91ab4d6f0cfb84adb42c0ac84
Pratham Shah <pratham.shah@icreativetechnolabs.com>
2025-03-25 19:07:52 +0530

    [ADD]Custom Qweb report

--- ak_library_management/__manifest__.py
+++ ak_library_management/__manifest__.py
@@ -10,14 +10,14 @@
     'category': 'Library_management/Library_management',
     'website': 'https://www.aktivsoftware.com',
     'depends': [
-        'stock', 'web', 'sale_management','base_automation','hr'
+        'stock', 'web', 'sale_management','base_automation','hr','website',
     ],
     'data': [
         'security/res_groups.xml',
         'security/ir.model.access.csv',
         'reports/library_location_report_action.xml',
         'reports/library_location_report.xml',
-        'reports/sale_order_report.xml',
+        'reports/report_invoice_inherit.xml',
         'views/library_book.xml',
         'views/library_member.xml',
         'views/library_book_tag.xml',
@@ -30,6 +30,7 @@
         'views/borrow_transaction_history.xml',
         'views/res_partner.xml',
         'views/res_partner_template.xml',
+        'views/contact_form.xml',
         'views/stock_warehouse.xml',
         'views/res_config_setting.xml',
         'wizard/bulk_book.xml',
@@ -39,11 +40,11 @@
         'data/ir_cron.xml',
         'data/mail_template_data.xml',
     ],
-    'assets': {
-        'web.assets_frontend': [
-            'ak_library_management/static/src/scss/contactus.scss',
-        ],
-    },
+    # 'assets': {
+    #     'web.assets_frontend': [
+    #         'ak_library_management/static/src/scss/contactus.scss',
+    #     ],
+    # },
     'installable': True,
     'license': 'LGPL-3',
 }
--- ak_library_management/controller/contact_list.py
+++ ak_library_management/controller/contact_list.py
@@ -1,19 +1,51 @@
 from odoo import http
 from odoo.http import request,route
+import unidecode
+import re
+# from odoo.addons.website.models.website import slug
 
-class ResPartnerController(http.Controller):
-    @http.route('/contactus', auth='public', website=True)
-    def contact_list(self):
-        partners = request.env['res.partner'].search([])
-        return request.render('ak_library_management.res_partner_template',{
-            'partners': partners
-        })
+# class ResPartnerController(http.Controller):
+#     @http.route('/contactus', auth='public', website=True)
+#     def contact_list(self):
+#         partners = request.env['res.partner'].search([])
+#         return request.render('ak_library_management.res_partner_template',{
+#             'partners': partners
+#         })
+#
+#     @http.route('/contactus/<int:partner_id>', auth='public', website=True)
+#     def contact_detail(self, partner_id):
+#         partner = request.env['res.partner'].browse(partner_id)
+#         if not partner.exists():
+#             return request.not_found()
+#         return request.render('ak_library_management.res_partner_detail_template', {
+#             'partner': partner
+#         })
 
-    @http.route('/contactus/<int:partner_id>', auth='public', website=True)
-    def contact_detail(self, partner_id):
-        partner = request.env['res.partner'].browse(partner_id)
-        if not partner.exists():
+class ContactsController(http.Controller):
+
+    def generate_slug(self, name, record_id):
+        """Generate a URL-safe slug from the contact name and ID."""
+        slug = unidecode.unidecode(name).lower()
+        slug = re.sub(r'\W+', '-', slug)  # Replace non-alphanumeric characters with dashes
+        return f"{slug}-{record_id}"
+
+    @http.route('/contacts', type='http', auth="public", website=True)
+    def contacts_page(self, **kwargs):
+        """Render the Kanban-style contacts page."""
+        contacts = request.env['res.partner'].sudo().search([])
+        for contact in contacts:
+            contact.slug = self.generate_slug(contact.name, contact.id)
+        return request.render("ak_library_management.contacts_page", {'contacts': contacts})
+
+    @http.route('/contact/<string:slug>', type='http', auth="public", website=True)
+    def contact_detail(self, slug, **kwargs):
+        """Display contact details using slug."""
+        contact_id = slug.split('-')[-1]  # Extract the last part as ID
+        contact = request.env['res.partner'].sudo().browse(int(contact_id))
+
+        if not contact.exists():
             return request.not_found()
-        return request.render('ak_library_management.res_partner_detail_template', {
-            'partner': partner
-        })
+
+        return request.render("ak_library_management.contact_detail", {'contact': contact})
+
+
