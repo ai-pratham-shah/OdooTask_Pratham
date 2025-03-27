eb946fd5d7f0d1e40bec29ba51c732097056ba75
Pratham Shah <pratham.shah@icreativetechnolabs.com>
2025-03-27 09:59:47 +0530

    [ADD]|Modify Existing/Base Qweb reports Assignment

--- ak_library_management/__manifest__.py
+++ ak_library_management/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'library management',
-    'version': '18.0.1.2.0',
+    'version': '18.0.1.3.0',
     'author' : 'Pratham shah',
     'summary': 'Library management module',
     'description': """
