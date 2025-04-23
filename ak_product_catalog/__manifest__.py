a3ad93d8054c7b9ea5956a6d01be77296f030e7a
Pratham Shah <pratham.shah@icreativetechnolabs.com>
2025-04-23 12:56:09 +0530

    [UPD]|Remove unnecessary comments

--- ak_product_catalog/__manifest__.py
+++ ak_product_catalog/__manifest__.py
@@ -5,9 +5,7 @@
     'summary': 'Generate customized PDF product catalogs',
     'description': """
         Product Catalog PDF Generator
-        ============================
-        This module adds a wizard to generate customized product catalogs in PDF format
-        with different styles and layouts.
+       
     """,
     'website': 'https://www.aktivsoftware.com',
     'depends': ['sale', 'product', 'stock','sale_management'],
@@ -20,9 +18,7 @@
         'reports/report_product_catalog.xml',
 
     ],
-    # 'images': ['static/description/icon.png'],
     'installable': True,
     'application': False,
-    # 'auto_install': False,
     'license': 'LGPL-3',
 }
